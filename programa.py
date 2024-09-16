ecfb6ace71b60c8de4ccff5d6802e4f1bbe91961
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2024-09-16 14:46:39 -0300

    codigo inicial

---
+++ programa.py
@@ -0,0 +1,111 @@
+from machine import Pin, ADC
+import time
+import rp2
+from rp2 import PIO
+from machine import Pin
+from time import sleep
+
+# Pines
+sensor_pin = ADC(Pin(26))  
+led_rojo = Pin(16, Pin.OUT)  
+led_verde = Pin(17, Pin.OUT) 
+led_azul = Pin(18, Pin.OUT)  
+
+umbral_luz = 40000  
+
+# Código predefinido para comparar
+codigo_correcto = "1234"
+codigo_ingresado = ""
+dia = True
+candado = False
+
+# Función para manejar la entrada de teclas
+def oninput(machine):
+    global codigo_ingresado
+    global dia
+    global candado
+    keys = machine.get()
+    while machine.rx_fifo():
+        keys = machine.get()
+    
+    pressed = []
+    for i in range(len(key_names)):
+        if (keys & (1 << i)):
+            pressed.append(key_names[i])
+    
+    if dia:
+        print("El LED está en rojo, ignorando entrada de teclas.")
+
+    else:
+        if len(pressed) > 0:
+            codigo_ingresado += pressed[0]  
+            print("Código ingresado hasta ahora:", codigo_ingresado)
+            
+            if len(codigo_ingresado) == 4:
+                if codigo_ingresado == codigo_correcto:
+                    print("Código correcto. Cambiando a verde.")
+                    candado = not candado
+                else:
+                    print("Código incorrecto. Reiniciando.")
+                codigo_ingresado = ""  
+
+
+@rp2.asm_pio(set_init=[PIO.IN_HIGH]*4)
+def keypad():
+    wrap_target()
+    set(y, 0)                             # 0
+    label("1")
+    mov(isr, null)                        # 1
+    set(pindirs, 1)                       # 2
+    in_(pins, 4)                          # 3
+    set(pindirs, 2)                       # 4
+    in_(pins, 4)                          # 5
+    set(pindirs, 4)                       # 6
+    in_(pins, 4)                          # 7
+    set(pindirs, 8)                       # 8
+    in_(pins, 4)                          # 9
+    mov(x, isr)                           # 10
+    jmp(x_not_y, "13")                    # 11
+    jmp("1")                              # 12
+    label("13")
+    push(block)                           # 13
+    irq(0)
+    mov(y, x)                             # 14
+    jmp("1")                              # 15
+    wrap()
+
+for i in range(10, 14):
+    Pin(i, Pin.IN, Pin.PULL_DOWN)
+
+key_names = "*7410852#963DCBA"
+
+sm = rp2.StateMachine(0, keypad, freq=2000, in_base=Pin(10, Pin.IN, Pin.PULL_DOWN), set_base=Pin(6))
+sm.active(1)
+sm.irq(oninput)
+
+print("Por favor, ingrese un código en el teclado numérico, o presione Ctrl+C para ingresar al REPL.")
+
+# Bucle principal
+while True:
+    valor_luz = sensor_pin.read_u16()
+    # print(valor_luz)
+    #print(valor_luz)
+
+
+    if not candado:
+      if valor_luz > umbral_luz:
+ 
+          led_rojo.on()
+          led_verde.off()
+          led_azul.off()  
+          dia = True
+      else:
+          led_rojo.off()
+          led_verde.off()
+          led_azul.on()
+          dia = False
+    else:
+      led_rojo.off()
+      led_verde.on()
+      led_azul.off()
+    time.sleep(0.1)
